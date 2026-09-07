420edc95d4aaa3cc7cc7ca9cbf9c79f71b78aa9d
Mike <fearmikey@users.noreply.github.com>
2026-09-07 02:53:40 -0500

    Added app icon

--- app/src/main/java/com/fearmikey/garage/ui/dashboard/DashboardScreen.kt
+++ app/src/main/java/com/fearmikey/garage/ui/dashboard/DashboardScreen.kt
@@ -1,5 +1,8 @@
 package com.fearmikey.garage.ui.dashboard
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
@@ -11,16 +14,19 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.fearmikey.garage.R
 import com.fearmikey.garage.ui.components.EmptyState
 import com.fearmikey.garage.ui.components.VehicleCard
 import com.fearmikey.garage.ui.theme.GarageTheme
@@ -120,3 +126,6 @@ private fun DashboardScreenEmptyPreview() {
         )
     }
 }
+
+
+
