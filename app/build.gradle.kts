5dd2a76fa2bcbe2a4f6022066809ee2c6b709fbc
Mike <fearmikey@users.noreply.github.com>
2026-09-20 09:36:00 -0500

    Disable dependenciesInfo in APK to comply with F-Droid signature checks

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,6 +66,10 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+    dependenciesInfo {
+        includeInApk = false
+        includeInBundle = false
+    }
     buildFeatures {
         compose = true
         buildConfig = true
